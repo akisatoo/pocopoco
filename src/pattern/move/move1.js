9082af50505e111e0acf4c0f6aef6f04ef8cf5b9
KAZU <KAZU@shoukoukazuhiro-no-MacBook-Air.local>
2015-05-26 18:54:41 +0900

    move1の修正

--- src/pattern/move/move1.js
+++ src/pattern/move/move1.js
@@ -7,7 +7,7 @@ App.pattern.move = App.pattern.move || [];
 	/**
 	 * 移動パターン
 	 * 
-	 * 敵を追尾する（加速つき）
+	 * 敵を追尾（加速つき）
 	 * id: 1
 	 */
 	App.pattern.move[1] = function (config) {
@@ -19,90 +19,30 @@ App.pattern.move = App.pattern.move || [];
 
 			id: 1,
 			type:  'homing_acceleration',
+			addSpeed: 0,
+			speedMax: 8,
 
-			/**
-			 ** 移動法
-			 */
 			_updateMove: function (config) {
 				config = config || {};
-
-				//console.log(disPos);			
+		
 				var self = this;
 				var target = config.target;
-				var disX = target.x - self.chara.x;
-				var disY = target.y - self.chara.y;
+				var speed = 2;
+				//var addSpeed = speed;
+				//var speedMax = 10;
+
+				//radianをだす
+				var rad = Math.atan2(target.y - self.chara.y, target.x - self.chara.x);
 				
-				var aRotSpeed = 0.1;	// 弾の回転加速度
-				var RotSpeedMax = 100;	// 弾の最高回転速度
-
-				var _x = self.chara.x;
-				var _y = self.chara.y;
-
-				// ターゲットとのラジアンを求める
-				var rad = Math.atan2(target.y - _y,target.x - _x);
-
-				var rot = rad * 180 / Math.PI;	// ラジアンから角度に変換
-				var _rotation = rot;		// 回転
-
-				var RotSpeed = 10;	// 回転速度
-				var Speed = 6;		// スピード
-
-				// 的と弾の角度
-				rad = Math.atan2(target.y - _y, target.x - _x);
-				rot = rad * 180 / Math.PI;	// ラジアンに変換
-
-				// 角度を丸める
-				if (_rotation - 180 > rot) {
-					rot += 360;
-				}
-				if (_rotation + 180 < rot) {
-					rot -= 360;
-				}
-
-				// 現在の角度と比較して近い方に少し回転
-				if (_rotation - rot > RotSpeedMax) {
-					RotSpeed -= aRotSpeed;
-				} else if (_rotation - rot < -RotSpeedMax) {
-					RotSpeed += aRotSpeed;
+				if(self.addSpeed <= self.speedMax){
+					self.addSpeed += speed * 0.05;
 				} else {
-					RotSpeed *= 0.5;	// 減速
+					self.addSpeed = self.speedMax;
 				}
-
-				// 回転速度のリミッタ
-				if (RotSpeed > RotSpeedMax)	RotSpeed = RotSpeedMax;
-				if (-RotSpeed < -RotSpeedMax)	RotSpeed = RotSpeedMax;
-
-				// 角度に回転速度を加算  
-				_rotation += RotSpeed;
-
-				Speed = Speed * 2.05;	// スピードを加速
-
-				// 的と弾の角度
-				var rad2 = _rotation * Math.PI / 180;
-
-				// 移動量を計算
-				var dx = Math.cos(rad2) * Speed;
-				var dy = Math.sin(rad2) * Speed;
-
-
-				if (disX > 0) {
-					//ターゲットが右
-					self.chara.y += dy;
-				} else if (disX < 0) {
-					//ターゲットが左
-					self.chara.y -= dy;
-				}
-
-				if (disX > 0) {
-					//ターゲットが右
-					self.chara.x += dx;
-				} else if (disX < 0) {
-					//ターゲットが左
-					self.chara.x -= dx;
-				}		
-
-				//アニメーションの変更
-				self._changeAnimPattern({}, {target: target});
+				console.log(self.addSpeed);
+				
+				self.chara.x += speed * Math.cos(rad) * self.addSpeed;
+				self.chara.y += speed * Math.sin(rad) * self.addSpeed;	
 
 				return;
 			},
